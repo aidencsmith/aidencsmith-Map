38b3402246bf0483b39d93082e562e08475d1a84
aidencsmith <acsmith7643@gmail.com>
2023-02-09 18:11:44 +0000

    map commit

--- src/main/java/MapExercise.java
+++ src/main/java/MapExercise.java
@@ -27,7 +27,9 @@ public class MapExercise {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+
+        HashMap<Integer, String> myMap = new HashMap<Integer, String>();
+        return myMap;
     }
 
     /**
@@ -37,7 +39,7 @@ public class MapExercise {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        return map.size();
     }
 
     /**
@@ -48,6 +50,8 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+    
+        map.put(key, value);
     }
 
     /**
@@ -57,7 +61,7 @@ public class MapExercise {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        return map.get(key);
     }
 
     /**
@@ -67,6 +71,8 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        
+        map.remove(key);
     }
 
     /**
@@ -77,6 +83,6 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
-
+        map.put(key, value);
     }
 }
